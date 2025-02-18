ef8d388f333ba4f5731675578ad4cabaad3f4950
Guilherme Torres <guiatorres8@gmail.com>
2025-02-18 03:01:09 -0300

    buttons stylized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import { Button } from "@/components/button";
+import { ArrowRight, Copy } from 'lucide-react'
+import { IconButton } from "@/components/icon-button";
+
 export default function Home() {
   return (
     <main>
-      <div>Hello World!</div>
+
+      <div className="">Hello World!</div>
+      <Button text="Guilherme">
+        Enviar
+        <ArrowRight/>
+      </Button>
+
+      <IconButton>
+        <Copy/>
+      </IconButton>
+
     </main>
   );
 }
---
+++ src/components/button.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react"
+
+interface ButtonProps {
+  text?: string
+  children: ReactNode
+}
+
+export function Button(props: ButtonProps) {
+  return (
+    <button className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration 300" >
+      {props.children || "Enviar"}
+    </button>
+  )
+}
